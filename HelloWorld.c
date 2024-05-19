4b3f9bc3786df08f29267b6dd2c63e73060229b8
deba33 <deba.dbs.bkt@gmail.com>
2024-05-19 18:12:59 +0530

    chapter 4: loops added

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+// calculate sum from 5 to 50 incl 5 & 50
 int main()
 {
-    printf("Hello World\n");
+
+    int sum = 0;
+    for (int i = 5; i <= 50; i++)
+    {
+        sum = sum + i;
+    }
+    printf("%d", sum);
+
     return 0;
 }
